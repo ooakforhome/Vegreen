ea1ba30f49d88d3fd190688ca4ebb7cb7bc23795
mithsin <hsinbluemoon@gmail.com>
2018-10-22 13:12:07 -0400

    updates

--- client/app/components/pages/menus.js
+++ client/app/components/pages/menus.js
@@ -10,18 +10,18 @@ class Menus extends Component {
       magzImg  : "/api/image/AtlMag.jpg",
       newArrImg: "/api/image/NewArrival.jpg",
       showPopup: false,
-      menu1: "/api/image/menu1.jpg",
-      menu2: "/api/image/menu2.jpg",
-      menuSushi: "/api/image/menuSushi.jpg",
-      menuLunch: "/api/image/menuLunch.jpg",
-      menu1L: "/api/image/menu1L.jpg",
+      menu1: "/api/image/menu1.JPG",
+      menu2: "/api/image/menu2.JPG",
+      menuSushi: "/api/image/menuSushi.JPG",
+      menuLunch: "/api/image/menuLunch.JPG",
+      menu1L: "/api/image/menu1L.JPG",
       menu2L: "/api/image/menu2L.jpg",
       menuSushiL: "/api/image/menuSushiL.jpg",
       menuLunchL: "/api/image/menuLunchL.jpg",
-      dish1 : "/api/image/dish-01-360px-240px",
-      dish2 : "/api/image/dish-02-360px-240px",
-      dish3 : "/api/image/dish-03-360px-240px",
-      dish4 : "/api/image/dish-04-360px-240px",
+      dish1 : "/api/image/dish-01-360px-240px.jpg",
+      dish2 : "/api/image/dish-02-360px-240px.jpg",
+      dish3 : "/api/image/dish-03-360px-240px.jpg",
+      dish4 : "/api/image/dish-04-360px-240px.JPG",
       text1: "Vegreen\'s menu is carefully designed for your healthy consumption and we have the best flavor you can imagine\. Moreover, We bring out the flavor of the world\. We want to open up your mind\, and show you that vegetarian has no boundary\. From Asia\, to Europe\, to North and South America\, or to Australia\, \"Any type of dish can now be replaced by vegetarian\"\, and it will taste as good as the original dish if not better\."
     }
   }
@@ -31,7 +31,7 @@ class Menus extends Component {
     e.preventDefault();
     this.setState({
       showPopup: !this.state.showPopup,
-      img: e.target.src + "L",
+      img: e.target.alt,
       name: e.target.name
     });
   }
@@ -52,43 +52,39 @@ class Menus extends Component {
           </div>
           <div className="s-col-12 col-4">
             <div className="title_logo">
-              <img alt="mag" src={this.state.awardImg} />
+              <img className="mag_img" alt="mag" src={this.state.awardImg}/>
             </div>
           </div>
         </div>
         <div className="col-12 text_center">
-          <p className="menu_text">{this.state.text1}</p>
+          <p className="menu_text iCol-7 s-iCol-9">{this.state.text1}</p>
         </div>
         <div className="menu_box">
           <div className="s-col-6 col-3 menu-thumb">
             <img
-              alt="menu_01"
               src={this.state.menu1}
-              value={this.state.menu1}
+              alt={this.state.menu1L}
               onClick={this.togglePopup.bind(this)}/>
             <p>MENU 01</p>
           </div>
           <div className="s-col-6 col-3 menu-thumb">
             <img
-              alt="menu_02"
               src={this.state.menu2}
-              value={this.state.menu2}
+              alt={this.state.menu2L}
               onClick={this.togglePopup.bind(this)}/>
             <p>MENU 02</p>
           </div>
           <div className="s-col-6 col-3 menu-thumb">
             <img
-              alt="Sushi"
               src={this.state.menuSushi}
-              value={this.state.menuSushi}
+              alt={this.state.menuSushiL}
               onClick={this.togglePopup.bind(this)}/>
             <p>SUSHI MENU</p>
           </div>
           <div className="s-col-6 col-3 menu-thumb">
             <img
-              alt="Lunch"
               src={this.state.menuLunch}
-              value={this.state.menuLunch}
+              alt={this.state.menuLunchL}
               onClick={this.togglePopup.bind(this)}/>
             <p>LUNCH MENU</p>
           </div>

6d1684bb8a1da2ba7057b56f67c1a747fd9d7a19
mithsin <hsinbluemoon@gmail.com>
2019-05-09 04:01:59 -0400

    update

---
+++ client/app/components/pages/deleteThis.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Delete extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <p>Hello delete me</p>
+      </div>
+    )
+  }
+}
+
+export default Delete
